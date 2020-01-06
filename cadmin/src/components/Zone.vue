e9462c758e8150be7448dbb98f3978b11f994ade
seongyu <suengyu@me.com>
2020-01-06 23:42:06 +0900

    c

--- cadmin/src/components/Zone.vue
+++ cadmin/src/components/Zone.vue
@@ -1,105 +1,118 @@
 <template>
-  <TableLayout :title="title" :lists="lists">
-    <template v-slot:cheader>
-      <div class="input-group">
-        <div class="input-group-prepend">
-          <span class="input-group-text" id="inputGroup-sizing-default">
-            <i class="glyphicon glyphicon-search"></i>
-          </span>
-        </div>
-        <input
-          type="text"
-          class="form-control"
-          aria-label="Default"
-          placeholder="Search"
-          aria-describedby="inputGroup-sizing-default"
-          v-model="search"
-        />
-      </div>
-    </template>
-    <template v-slot:cbody>
-      <tr>
-        <th v-for="title in lists.title" v-bind:key="title.title">{{title}}</th>
-      </tr>
-      <tr v-for="(item,i) in lists.item" v-bind:key="item.$id">
-        <td>{{i+1}}</td>
-        <td>{{item.tag}}</td>
-        <td>{{item.flow}}</td>
-        <td>{{item.area}}</td>
-        <td>{{item.map}}</td>
-        <td>{{item.insertor}}</td>
-        <td>{{item.insertDt}}</td>
-        <td>{{item.editor}}</td>
-        <td>{{item.editDt}}</td>
-        <td>{{item.etc}}</td>
-        <td>EXECUTE</td>
-      </tr>
-    </template>
-    <template v-slot:cfoot>
-      <nav aria-label="Page navigation">
-        <ul class="pagination justify-content-center">
-          
-          <li class="page-item" v-for="n in lists.tpage" v-bind:key="n">
-            <a class="page-link" href="#" v-on:click="pageEvent(n)">{{n}}</a>
-          </li>
-        </ul>
-      </nav>
-    </template>
-  </TableLayout>
+    <TableLayout :title="title" :lists="lists">
+        <template v-slot:cheader>
+            <div class="input-group">
+                <div class="input-group-prepend">
+                    <span class="input-group-text" id="inputGroup-sizing-default">
+                        <i class="glyphicon glyphicon-search"></i>
+                    </span>
+                </div>
+                <input type="text" class="form-control" aria-label="Default" placeholder="Search" aria-describedby="inputGroup-sizing-default" v-model="search" />
+            </div>
+        </template>
+        <template v-slot:cbody>
+            <tr>
+                <th v-for="title in lists.title" v-bind:key="title.title">{{loc[title]}}</th>
+            </tr>
+            <tr v-for="(item,i) in lists.item" v-bind:key="item.$id">
+                <td>{{i+1}}</td>
+                <td>{{item.zone_id}}</td>
+                <td>{{item.map_name}}</td>
+                <td>{{item.enabled}}</td>
+                <td>{{item.distance}}</td>
+                <td>{{item.installed_date}}</td>
+                <td>{{item.edit_date}}</td>
+                <td>EXECUTE</td>
+            </tr>
+        </template>
+        <template v-slot:cfoot>
+            <nav aria-label="Page navigation">
+                <ul class="pagination justify-content-center">
+                    <li class="page-item" v-for="n in lists.tpage" v-bind:key="n">
+                        <a class="page-link" href="#" v-on:click="pageEvent(n)">{{n}}</a>
+                    </li>
+                </ul>
+            </nav>
+        </template>
+    </TableLayout>
 </template>
-
 <script>
 import TableLayout from "../layout/TableLayout.vue";
 import moment from "moment";
-import $ from 'jquery';
+import _util from "../assets/util.js";
+import localize from "../assets/localization.json";
+import $ from "jquery";
 
 export default {
-  name: 'Zone',
-  components:{TableLayout},
-  data() {
-    return {
-      title: "Zone",
-      search:'',
-      lists:{title:[
-        '순번','태그','층','구역','맵','등록자','등록일','갱신자','갱신일','비고','실행'
-      ],
-      tpage:1,
-      page:1,
-      item:[
-        {
-  tag : 'CCC',
-  flow : 1,
-  area : 'exit',
-  map : '1F',
-  insertor : 'someone',
-  insertDt : moment(new Date()).format("YYYY-MM-DD"),
-  editor : 'someone',
-  editDt : moment(new Date()).format("YYYY-MM-DD"),
-  etc : ''
-}
-      ]}
-    };
-  },
-  methods : {
-    pageEvent : (n)=>{
-      var pgl = $('li.page-item');
-      if(pgl.length==0){
-        return null;
-      }else{
-        for(var i in pgl){
-        if(i==n-1){
-            $(pgl[i]).addClass('active')
-          }else{
-            $(pgl[i]).removeClass('active')
-          }
+    name: 'Zone',
+    components: { TableLayout },
+    data() {
+        return {
+            title: "Zone",
+            search: '',
+            loc: localize,
+            lists: {
+                title: [
+                    'index', 'zone_id', 'map_name', 'enabled', 'distance', 'installed_date', 'edit_date', 'exec'
+                ],
+                tpage: 1,
+                page: 1,
+                item: [{
+                    zone_id: null,
+                    map_name: null,
+                    enabled: null,
+                    distance: null,
+                    installed_date: null,
+                    edit_date: null
+                }]
+            }
+        };
+    },
+    async mounted() {
+        var _url = [this.$apiUrl, '/map/zone'].join("");
+        var _rtn = await this.$http.get(_url);
+
+        if (_rtn.status != 200 & _rtn.data.length <= 0) {
+            return null;
         }
-      }
-      
+        var items = [],
+            item, _it, i;
+        try {
+            for (i in _rtn.data) {
+                _it = _rtn.data[i];
+                item = {
+                    zone_id: _it.zone_id,
+                    map_name: _it.name,
+                    enabled: _it.enabled ? '활성' : '비활성',
+                    distance: _it.real_distance_factor,
+                    installed_date: _it.created_time ? moment(_util.timeparser(_it.created_time)).format("YYYY-MM-DD") : null,
+                    edit_date: _it.modified_time ? moment(_util.timeparser(_it.modified_time)).format("YYYY-MM-DD") : null
+                }
+                items.push(item);
+            }
+        } catch (e) {
+            console.log(e);
+        }
+        this.lists.item = items;
+    },
+    methods: {
+        pageEvent: (n) => {
+            var pgl = $('li.page-item');
+            if (pgl.length == 0) {
+                return null;
+            } else {
+                for (var i in pgl) {
+                    if (i == n - 1) {
+                        $(pgl[i]).addClass('active')
+                    } else {
+                        $(pgl[i]).removeClass('active')
+                    }
+                }
+            }
+
+        }
     }
-  }
 }
 </script>
-
 <style scoped>
-
 </style>
--- interface/apis/map.js
+++ interface/apis/map.js
@@ -6,54 +6,68 @@ const queryset = require('../database/query');
 const router = express.Router();
 
 var handler = {
-    getLists : async (req,res) => {
-    },
-    select : async (req,res) => {
+    getLists: async (req, res) => {},
+    select: async (req, res) => {
         res.send({});
     },
-    insert : async (req,res) => {
+    insert: async (req, res) => {
         res.send({});
     },
-    update : async (req,res) => {
+    update: async (req, res) => {
         res.send({});
     },
-    delete : async (req,res) => {
+    delete: async (req, res) => {
         res.send({});
     },
-    getPosition : async (req,res)=>{
+    getPosition: async (req, res) => {
         var rs, _tag_id, _sql;
-        try{
+        try {
             _tag_id = req.params.tagId;
-            _sql = util.format(queryset['map']['getTagPosition'],_tag_id);
+            _sql = util.format(queryset['map']['getTagPosition'], _tag_id);
             rs = await dbcontrol.query(_sql);
-        }catch(e){
+        } catch (e) {
             rs = false;
-        }finally{
+        } finally {
             res.send(rs);
         }
     },
-    getZoneList : async (req,res) => {
-        var rs, _data;
-        try{
+    getZoneList: async (req, res) => {
+        var rs, _data, items, _r, i, _sql,_k,j;
+        try {
             _data = {
-                table : 'zone'
+                table: 'zone'
             }
-            rs = await dbcontrol.defaultQuery(_data,'get');
+            rs = await dbcontrol.defaultQuery(_data, 'get');
+            items = rs.data;
+            if (items && items.length > 0) {
+                for (i in items) {
+                    _sql = util.format(queryset.map.getMapByZone,items[i].map_id);
+                    _r = await dbcontrol.query(_sql);
+                    if(!_r||!_r.data||_r.data.length<=0) continue;
+                    _r = _r.data[0];
+                    _k = Object.keys(_r);
+                    for(j in _k){
+                        items[i][_k[j]] = _r[_k[j]]
+                    }
+                }
+            }
+            rs = items;
             console.log(rs);
-        }catch(e){
+        } catch (e) {
+            console.log(e);
             rs = false;
-        }finally{
+        } finally {
             res.send(rs);
         }
     }
 }
-router.get('/zone',handler.getZoneList)
-router.get('/tag/position/:tagId',handler.getPosition)
+router.get('/zone', handler.getZoneList)
+router.get('/tag/position/:tagId', handler.getPosition)
 
 // CRUD set
-router.get('/:mapId',handler.select);
-router.post('/:mapId',handler.update);
-router.delete('/:mapId',handler.delete);
+router.get('/:mapId', handler.select);
+router.post('/:mapId', handler.update);
+router.delete('/:mapId', handler.delete);
 router.post('/', handler.insert);
 
 router.get('/', handler.getLists);
